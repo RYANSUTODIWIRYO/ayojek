ddc134d323c03fde90f0b3e4d9b169c9e0f05561
Aisah Taufik <athaisyah@yahoo.com>
2020-09-25 00:10:22 +0700

    update register form

--- screen/registerForm/index.js
+++ screen/registerForm/index.js
@@ -1,16 +1,125 @@
-import React, { Component } from "react"
-import { View, Text } from "react-native"
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 
-class RegisterForm extends Component {
-    render() { 
-        return (
-            <View>
-                <Text>
-                    Ini register form 
-                </Text>
+export default class RegisterForm extends Component {
+  state = {
+    name: '',
+    email: '',
+    phone: '+62',
+  };
+
+  // Register
+  //   onPressRegister(){}
+
+  render() {
+    return (
+      <View style={{padding: 20, backgroundColor: 'white'}}>
+        {/* TITLE */}
+        <View style={{marginTop: 25}}>
+          <Text style={{fontSize: 25, fontWeight: 'bold'}}>Daftar</Text>
+          <Text>Lengkapi data dirimu di bawah ini, ya</Text>
+        </View>
+
+        {/* FORM */}
+        <View style={styles.form}>
+          <View>
+            <Text>
+              Nama Lengkap <Text style={{color: 'red'}}>*</Text>
+            </Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Cth: Name"
+              value={this.state.name}
+              onChangeText={(name) => this.setState({name})}></TextInput>
+          </View>
+
+          <View style={{marginTop: 25}}>
+            <Text>
+              Email <Text style={{color: 'red'}}>*</Text>
+            </Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Cth: name@email.com"
+              value={this.state.email}
+              onChangeText={(email) => this.setState({email})}></TextInput>
+          </View>
+
+          <View style={{marginTop: 25}}>
+            <Text>
+              Nomor HP <Text style={{color: 'red'}}>*</Text>
+            </Text>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignContent: 'center',
+                alignItems: 'center',
+              }}>
+              <View>
+                <Image style={styles.indonesia} source={images.indonesia} />
+              </View>
+              <View style={{flex: 1, marginLeft: 10}}>
+                <TextInput
+                  style={styles.input}
+                  placeholder="12345678"
+                  value={this.state.phone}
+                  onChangeText={(phone) => this.setState({phone})}></TextInput>
+              </View>
             </View>
-        )
-    }
+          </View>
+
+          {/* ONPRESS */}
+          <View
+            style={{
+              marginTop: 25,
+              justifyContent: 'flex-end',
+              alignItems: 'flex-end',
+            }}>
+            <TouchableOpacity
+            // onPress={onPressRegister}
+            >
+              <Image style={styles.button} source={images.forward} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+  }
 }
- 
-export default RegisterForm;
+
+const images = {
+  indonesia: require('../../asset/image/indonesia.png'),
+  forward: require('../../asset/image/forward-button.png'),
+};
+
+const styles = StyleSheet.create({
+  form: {
+    marginTop: 25,
+  },
+  inputTitle: {
+    color: '#8a8f9e',
+    fontSize: 10,
+  },
+  input: {
+    borderBottomColor: '#8a8f9e',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    // height: 40,
+    fontSize: 18,
+    color: '#161f3d',
+  },
+  indonesia: {
+    width: 28,
+    height: 28,
+  },
+  button: {
+    height: 38,
+    width: 38,
+  },
+});
