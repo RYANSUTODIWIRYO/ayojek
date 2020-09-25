14f218fb74fec4c2e419df39291599063742c6f7
RYANSUTODIWIRYO <ryan.s.sutodiwiryo@gmail.com>
2020-09-25 18:17:28 +0700

    update login with phone number

--- android/app/src/main/java/com/ayojek/MainApplication.java
+++ android/app/src/main/java/com/ayojek/MainApplication.java
@@ -11,6 +11,9 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+// import io.invertase.firebase.RNFirebasePackage;
+// import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -26,6 +29,10 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+
+          // packages.add(new RNFirebasePackage());
+          // packages.add(new RNFirebaseAuthPackage());
+
           return packages;
         }
 
--- screen/loginForm/index.js
+++ screen/loginForm/index.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import auth from '@react-native-firebase/auth';
+// import firebase from 'react-native-firebase'
+
 import {
   StyleSheet,
   View,
@@ -10,25 +13,105 @@ import {
 
 export default class LoginForm extends Component {
   state = {
-    name: '',
-    email: '',
-    phone: '+62',
+    phone: "",
+    code: "",
+    confirm: ""
   };
 
+  // Login with phone number
+  onPressLogin = async () => {
+    console.log('phone:', this.state.phone)
+
+    const confirmation = await auth().signInWithPhoneNumber(this.state.phone)
+      .catch(error => {
+        console.log('Error Login: ', error)
+      })
+
+    console.log("confirmation: ", confirmation )
+    this.setState({
+      confirm: confirmation
+    })
+  }
+
+  onPressConfirmCode = async () => {
+    const { code, confirm } = this.state
+    try {
+      await confirm.confirm(code)
+      console.log('suksess', suksess)
+    } catch (error) {
+      console.log("Invalid Code :", error)
+    }
+  }
+
   render() {
-    return (
+    const { phone, confirm } = this.state
+    
+    if (!confirm) {
+    // if (false) {
+      return (
+        <View style={{padding: 20, backgroundColor: 'white'}}>
+          {/* TITLE */}
+          <View style={{marginTop: 25}}>
+            <Text style={{fontSize: 25, fontWeight: 'bold'}}>Masuk</Text>
+            <Text>Silakan masuk dengan nomor HP-mu yang terdaftar</Text>
+          </View>
+
+          {/* FORM */}
+          <View style={styles.form}>
+            <View style={{marginTop: 25}}>
+              <Text>
+                Nomor HP <Text style={{color: 'red'}}>*</Text>
+              </Text>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                  alignContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <View>
+                  <Image style={styles.indonesia} source={images.indonesia} />
+                </View>
+                <View style={{flex: 1, marginLeft: 10}}>
+                  <TextInput
+                    style={styles.input}
+                    placeholder="12345678"
+                    defaultValue="+62"
+                    keyboardType={"phone-pad"}
+                    onChangeText={(phone) => this.setState({phone})}></TextInput>
+                </View>
+              </View>
+            </View>
+
+            {/* ONPRESS */}
+            <View
+              style={{
+                marginTop: 25,
+                justifyContent: 'flex-end',
+                alignItems: 'flex-end',
+              }}>
+              <TouchableOpacity onPress={this.onPressLogin}>
+                <Image style={styles.button} source={images.forward} />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      )
+    }
+
+    return(
       <View style={{padding: 20, backgroundColor: 'white'}}>
         {/* TITLE */}
         <View style={{marginTop: 25}}>
-          <Text style={{fontSize: 25, fontWeight: 'bold'}}>Masuk</Text>
-          <Text>Silakan masuk dengan nomor HP-mu yang terdaftar</Text>
+          <Text style={{fontSize: 20, fontWeight: 'bold', marginBottom: 20}}>Kode OTP sudah dikirim!</Text>
+          <Text style={{textAlign: "justify"}}>Masukan kode OTP yang kami SMS ke nomor HP-mu yang terdaftar {phone}</Text>
         </View>
 
         {/* FORM */}
         <View style={styles.form}>
           <View style={{marginTop: 25}}>
             <Text>
-              Nomor HP <Text style={{color: 'red'}}>*</Text>
+              OTP <Text style={{color: 'red'}}>*</Text>
             </Text>
             <View
               style={{
@@ -37,15 +120,13 @@ export default class LoginForm extends Component {
                 alignContent: 'center',
                 alignItems: 'center',
               }}>
-              <View>
-                <Image style={styles.indonesia} source={images.indonesia} />
-              </View>
-              <View style={{flex: 1, marginLeft: 10}}>
+              <View style={{flex: 1}}>
                 <TextInput
                   style={styles.input}
-                  placeholder="12345678"
-                  value={this.state.phone}
-                  onChangeText={(phone) => this.setState({phone})}></TextInput>
+                  secureTextEntry={true}
+                  placeholder="●●●●"
+                  keyboardType={"number-pad"}
+                  onChangeText={(code) => this.setState({code})}></TextInput>
               </View>
             </View>
           </View>
@@ -57,15 +138,13 @@ export default class LoginForm extends Component {
               justifyContent: 'flex-end',
               alignItems: 'flex-end',
             }}>
-            <TouchableOpacity
-            // onPress={onPressRegister}
-            >
+            <TouchableOpacity onPress={this.onPressLogin}>
               <Image style={styles.button} source={images.forward} />
             </TouchableOpacity>
           </View>
         </View>
       </View>
-    );
+    )
   }
 }
 
@@ -84,10 +163,9 @@ const styles = StyleSheet.create({
   },
   input: {
     borderBottomColor: '#8a8f9e',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    // height: 40,
+    borderBottomWidth: 1,
     fontSize: 18,
-    color: '#161f3d',
+    color: '#161f3d'
   },
   indonesia: {
     width: 28,
